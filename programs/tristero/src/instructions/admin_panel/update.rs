792411ff13e4ed2aed813bb32f97837338cdb671
RustChainBuilder <freshtopromise@gmail.com>
2024-08-08 10:12:18 -0500

    update: admin_panel_update instruction

--- programs/tristero/src/instructions/admin_panel/update.rs
+++ programs/tristero/src/instructions/admin_panel/update.rs
@@ -3,15 +3,14 @@ use {anchor_lang::prelude::*, crate::error::*, crate::state::*};
 
 #[derive(Accounts)]
 pub struct Update<'info> {
-    #[account(mut,
-    constraint = admin_wallet.key() == admin_panel.admin_wallet @ CustomError::InvalidAuthority
-    )]
+    #[account(mut)]
     pub admin_wallet: Signer<'info>,
 
     #[account(
         mut,
         seeds = [b"admin_panel"],
-        bump
+        bump = admin_panel.admin_panel_bump,
+        has_one = admin_wallet
     )]
     pub admin_panel: Box<Account<'info, AdminPanel>>,
 }
@@ -25,7 +24,6 @@ pub struct UpdateParams {
 pub fn update(ctx: Context<Update>, params: &UpdateParams) -> Result<()> {
     let admin_panel = ctx.accounts.admin_panel.as_mut();
     admin_panel.admin_wallet = params.admin_wallet;
-    admin_panel.admin_panel_bump = ctx.bumps.admin_panel;
     admin_panel.payment_wallet = params.payment_wallet;
     Ok(())
 }
