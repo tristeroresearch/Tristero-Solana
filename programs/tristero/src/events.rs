084671a54b2cbeb54a70b9e38ccb918d27bef03d
RustChainBuilder <freshtopromise@gmail.com>
2024-08-21 08:10:59 -0500

    update: add MsgReceived in events

--- programs/tristero/src/events.rs
+++ programs/tristero/src/events.rs
@@ -1,8 +1,10 @@
 use crate::*;
 #[event]
-pub struct OFTReceived {
-    pub guid: [u8; 32],
+pub struct MsgReceived {
     pub src_eid: u32,
-    pub to: Pubkey,
-    pub amount_received_ld: u64,
+    pub sender: [u8; 32],
+    pub nonce: u64,
+    pub guid: [u8; 32],
+    pub message: Vec<u8>,
+    pub extra_data: Vec<u8>,
 }
