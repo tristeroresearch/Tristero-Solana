a9b49ff6b4e589e41549cc7a22eae7b6b3a3f93c
RustChainBuilder <freshtopromise@gmail.com>
2024-08-21 08:11:47 -0500

    update: remove BAB logic in receive instruction

--- programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
+++ programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
@@ -17,6 +17,7 @@ use oapp::{
 
 use solana_program::native_token::LAMPORTS_PER_SOL;
 
+#[event_cpi]
 #[derive(Accounts, Clone)]
 #[instruction(params: LzReceiveParams)]
 pub struct LzReceive<'info> {
@@ -98,113 +99,14 @@ impl LzReceive<'_> {
             trade_match.is_valiable = false;
 
         } else { // B->A->B
-            let receiver = msg_vec[3];
-            let mut remaining_accounts = ctx.remaining_accounts.to_vec();
-            // let tristero_oapp = accounts[0].clone();
-            let endpoint_program_id = remaining_accounts[7].clone();
-            let signer1 = remaining_accounts[10].clone();
-            let send_library_program_id = remaining_accounts[14].clone();
-            let price_fee_program_id = remaining_accounts[17].clone();
-            let price_fee_program_pda = remaining_accounts[18].clone();
-            remaining_accounts.insert(0, endpoint_program_id);
-            // remaining_accounts.insert(1, tristero_oapp);
-
-            remaining_accounts.insert(2, send_library_program_id);
-            remaining_accounts.insert(13, signer1);
-            remaining_accounts.extend_from_slice(&[
-                price_fee_program_id,
-                price_fee_program_pda
-            ]);
-
-            msg!("send_library_config: {:#?}", remaining_accounts[3].key());
-            msg!("default_send_library_config: {:#?}", remaining_accounts[4].key());
-            msg!("send_library_info: {:#?}", remaining_accounts[5].key());
-            msg!("nonce: {:#?}", remaining_accounts[7].key());
-
-
-
-            // --------------------------Send message through Oapp-----------------------------
-            let cpi_ctx = Send::construct_context(remaining_accounts[9].key(), remaining_accounts.as_ref()).unwrap();
-
-            let receive_options= [0, 3, 1, 0, 17, 1, 0,
-                    0, 0, 0, 0,  0, 0,   0,
-                    0, 0, 0, 0,  0, 7, 161,
-                    32]; // For lzReceiveOption
-            let sol_eid: u32 = 40168u32; // testnet(mainnet: 30168)
-
-            //message: to send to arbitrum
-            let mut message_to_send = Vec::<u8>::new();
-
-            //payload
-            for _ in 0..32 { // sender
-                message_to_send.push(0u8);
-            }
-            for _ in 0..28 {
-                message_to_send.push(0u8);
-            }
-            sol_eid.to_be_bytes().map(|value: u8| message_to_send.push(value)); // srcLzc
-            
-            for _ in 0..12 {
-                message_to_send.push(0u8);
-            }
-            trade_match.dest_token_mint.map(|value| message_to_send.push(value)); // erc20Token
-            trade_match.source_token_mint.to_bytes().map(|value| message_to_send.push(value)); // splToken
-            
-            for _ in 0..24 {
-                message_to_send.push(0u8);
-            }
-            trade_match.dst_index.to_be_bytes().map(|value| message_to_send.push(value)); // srcIndex(arb index)
-
-            for _ in 0..24 {
-                message_to_send.push(0u8);
-            }
-            trade_match.src_index.to_be_bytes().map(|value| message_to_send.push(value)); // dstIndex(sol index)
-
-            // for _ in 0..12 {
-            //     message_to_send.push(0u8);
-            // }
-            receiver.map(|value| message_to_send.push(value)); // taker
-            for _ in 0..24 {
-                message_to_send.push(0u8); 
-            }
-            trade_match.source_sell_amount.to_be_bytes().map(|value| message_to_send.push(value)); // minAmount
-            for _ in 0..31 {
-                message_to_send.push(0u8);
-            }
-            message_to_send.push(1u8); //status
-
-            for _ in 0..31 {
-                message_to_send.push(0u8);
-            }
-            message_to_send.push(1u8); // _msgType
-
-            let cpi_params = SendParams {
-                dst_eid: trade_match.eid,
-                receiver: receiver,
-                message: message_to_send, 
-                options: receive_options.to_vec(),
-                native_fee: LAMPORTS_PER_SOL * 3,
-                lz_token_fee: 0,
-            };
-            
-            endpoint::cpi::send(cpi_ctx.with_signer(signer_seeds), cpi_params)?;
-
-            // the first 9 accounts are for clear()
-            let accounts_for_clear = &remaining_accounts[0..2];
-            let _ = oapp::endpoint_cpi::clear(
-                ENDPOINT_ID,
-                remaining_accounts[1].key(),
-                accounts_for_clear,
-                &[b"TristeroOapp", &[admin_panel.bump]],
-                ClearParams {
-                    receiver: remaining_accounts[1].key(),
-                    src_eid: params.src_eid,
-                    sender: params.sender,
-                    nonce: params.nonce,
-                    guid: params.guid,
-                    message: params.message.clone(),
-                },
-            )?;
+            emit_cpi!(MsgReceived{
+                src_eid: params.src_eid,
+                sender: params.sender,
+                nonce: params.nonce,
+                guid: params.guid,
+                message: params.message.clone(),
+                extra_data: params.extra_data.clone(),
+            });
         }
         Ok(())
     }
