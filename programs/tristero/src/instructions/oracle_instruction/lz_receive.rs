080c9a5b73daa0e1e5b6392d7698c7a125a9eae1
RustChainBuilder <freshtopromise@gmail.com>
2024-08-22 08:51:01 -0500

    update: change return accounts in lz_receive_types instruction

--- programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
+++ programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
@@ -17,7 +17,7 @@ use oapp::{
 
 use solana_program::native_token::LAMPORTS_PER_SOL;
 
-#[event_cpi]
+
 #[derive(Accounts, Clone)]
 #[instruction(params: LzReceiveParams)]
 pub struct LzReceive<'info> {
@@ -29,30 +29,12 @@ pub struct LzReceive<'info> {
     )]
     pub oapp: Box<Account<'info, AdminPanel>>,
 
-    /// token account address
-    #[account(mut)]
-    pub token_account: Box<Account<'info, TokenAccount>>,
-
-    #[account(
-        mut,
-        seeds = [b"staking_account", token_account.mint.key().as_ref()],
-        bump,
-        
-    )]
-    pub staking_account: Box<Account<'info, TokenAccount>>,
-
     #[account(
         mut,
         constraint = trade_match.status == 0u8 @ CustomError::NotAgain
     )]
     pub trade_match: Box<Account<'info, TradeMatch>>,
-    
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account(
-        constraint = token_program.key() == TOKEN_PROGRAM_ID @ CustomError::InvalidTokenStandard,
-        constraint = token_account.mint.key() == trade_match.source_token_mint @ CustomError::InvalidTokenMintAddress
-    )]
-    pub token_program: AccountInfo<'info>,
+
 }
 
 #[derive(Clone, AnchorSerialize, AnchorDeserialize)]
@@ -68,46 +50,118 @@ pub struct LzReceiveParams {
 impl LzReceive<'_> {
     pub fn apply(ctx: &mut Context<LzReceive>, params: &LzReceiveParams) -> Result<()> {
         let mut accounts = ctx.accounts.clone().to_account_infos();
+        let mut remaining_accounts = ctx.remaining_accounts;
         let admin_panel = ctx.accounts.oapp.as_mut().clone();
-        
+        let signer_seeds: &[&[&[u8]]] = &[&[b"TristeroOapp", &[admin_panel.bump]]];
         let trade_match = ctx.accounts.trade_match.as_mut();
-        let msg_vec:Vec<[u8; 32]> = split_into_chunks(params.message.clone());
         
+        let msg_vec:Vec<[u8; 32]> = split_into_chunks(params.message.clone());
         let msg_type =  vec_to_u64(msg_vec[0]) % 16;
 
         msg!("msg_type: => {} {}", msg_type, vec_to_u64(msg_vec[0]));
-        
-        let stake_acc = ctx.accounts.staking_account.to_account_info();
-        let token_acc = ctx.accounts.token_account.to_account_info();
-        let authority = admin_panel.clone().to_account_info();
-
-        let signer_seeds: &[&[&[u8]]] = &[&[b"TristeroOapp", &[admin_panel.bump]]];
-
-        // ---------------------Transfer the source token from the staking account----------------------------------
-        let cpi_accounts = Transfer {
-            from: stake_acc,
-            to: token_acc,
-            authority: authority
-        };
-
-        let cpi_context = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
-        token::transfer(cpi_context.with_signer(signer_seeds), trade_match.source_sell_amount)?;
-
-        
-
         if msg_type == 1u64 && trade_match.status == 1u8 { // B->A
-            trade_match.status = 2u8;
+            let authority = remaining_accounts[0].to_account_info();
+            let stake_acc = remaining_accounts[1].to_account_info();
+            let token_acc = remaining_accounts[2].to_account_info();
 
-        } else { // B->A->B
+            
+
+            // ---------------------Transfer the source token from the staking account----------------------------------
+            let cpi_accounts = Transfer {
+                from: stake_acc,
+                to: token_acc,
+                authority: authority
+            };
+
+            let cpi_context = CpiContext::new(remaining_accounts[3].to_account_info(), cpi_accounts);
+            token::transfer(cpi_context.with_signer(signer_seeds), trade_match.source_sell_amount)?;
+
+
+            trade_match.status = 2u8;
+        } 
+        else { // B->A->B
+            let mut remaining_accounts = ctx.remaining_accounts.to_vec();
+            // let tristero_oapp = accounts[0].clone();
+            let endpoint_program_id = remaining_accounts[7].clone();
+            let signer1 = remaining_accounts[10].clone();
+            let send_library_program_id = remaining_accounts[14].clone();
+            let price_fee_program_id = remaining_accounts[17].clone();
+            let price_fee_program_pda = remaining_accounts[18].clone();
+            remaining_accounts.insert(0, endpoint_program_id);
+            // remaining_accounts.insert(1, tristero_oapp);
+
+            remaining_accounts.insert(2, send_library_program_id);
+            remaining_accounts.insert(13, signer1);
+            remaining_accounts.extend_from_slice(&[
+                price_fee_program_id,
+                price_fee_program_pda
+            ]);
+
+            // --------------------------Send message through Oapp-----------------------------
+            let cpi_ctx = Send::construct_context(remaining_accounts[9].key(), remaining_accounts.as_ref()).unwrap();
+
+            let receive_options= [0, 3, 1, 0, 17, 1, 0,
+                    0, 0, 0, 0,  0, 0,   0,
+                    0, 0, 0, 0,  0, 7, 161,
+                    32]; // For lzReceiveOption
+            let sol_eid: u32 = 40168u32; // testnet(mainnet: 30168)
+
+            //message: to send to arbitrum
+            let mut message_to_send = Vec::<u8>::new();
+
+            //payload
+            for _ in 0..32 { // sender
+                message_to_send.push(0u8);
+            }
+            for _ in 0..28 {
+                message_to_send.push(0u8);
+            }
+            sol_eid.to_be_bytes().map(|value: u8| message_to_send.push(value)); // srcLzc
+            
+            for _ in 0..12 {
+                message_to_send.push(0u8);
+            }
+            trade_match.dest_token_mint.map(|value| message_to_send.push(value)); // erc20Token
+            trade_match.source_token_mint.to_bytes().map(|value| message_to_send.push(value)); // splToken
+            
+            for _ in 0..24 {
+                message_to_send.push(0u8);
+            }
+            trade_match.dst_index.to_be_bytes().map(|value| message_to_send.push(value)); // srcIndex(arb index)
+
+            for _ in 0..24 {
+                message_to_send.push(0u8);
+            }
+            trade_match.src_index.to_be_bytes().map(|value| message_to_send.push(value)); // dstIndex(sol index)
+
+            let receiver = [0u8; 32];
+            receiver.map(|value| message_to_send.push(value)); // taker
+            for _ in 0..24 {
+                message_to_send.push(0u8); 
+            }
+            trade_match.source_sell_amount.to_be_bytes().map(|value| message_to_send.push(value)); // minAmount
+            for _ in 0..31 {
+                message_to_send.push(0u8);
+            }
+            message_to_send.push(1u8); //status
+
+            for _ in 0..31 {
+                message_to_send.push(0u8);
+            }
+            message_to_send.push(1u8); // _msgType
+
+            let cpi_params = SendParams {
+                dst_eid: trade_match.eid,
+                receiver: receiver,
+                message: message_to_send, 
+                options: receive_options.to_vec(),
+                native_fee: LAMPORTS_PER_SOL * 3,
+                lz_token_fee: 0,
+            };
+            
+            endpoint::cpi::send(cpi_ctx.with_signer(signer_seeds), cpi_params)?;
+
             trade_match.status = 1u8;
-            emit_cpi!(MsgReceived{
-                src_eid: params.src_eid,
-                sender: params.sender,
-                nonce: params.nonce,
-                guid: params.guid,
-                message: params.message.clone(),
-                extra_data: params.extra_data.clone(),
-            });
         }
         Ok(())
     }
