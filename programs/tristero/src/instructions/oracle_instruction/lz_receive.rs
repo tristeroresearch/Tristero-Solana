31c710397a8bed40947cfeaee211fa0234b8a556
RustChainBuilder <freshtopromise@gmail.com>
2024-08-26 11:21:23 -0500

    update: lz_receive, lz_receive_types instruction

--- programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
+++ programs/tristero/src/instructions/oracle_instruction/lz_receive.rs
@@ -21,7 +21,7 @@ pub struct LzReceive<'info> {
 
     #[account(
         mut,
-        // constraint = trade_match.status <= 1u8 @ CustomError::NotAgain
+        constraint = trade_match.status == 1u8 @ CustomError::NotAgain
     )]
     pub trade_match: Box<Account<'info, TradeMatch>>,
 
@@ -42,114 +42,24 @@ impl LzReceive<'_> {
         let remaining_accounts = ctx.remaining_accounts;
         let tristero_oapp_bump = ctx.bumps.oapp;
         let signer_seeds: &[&[&[u8]]] = &[&[b"TristeroOapp", &[tristero_oapp_bump]]];
-        
-        msg!("signer => {:#?}", signer_seeds);
+
         let trade_match = ctx.accounts.trade_match.as_mut();
-        
-        let msg_vec:Vec<[u8; 32]> = split_into_chunks(params.message.clone());
-        let msg_type =  vec_to_u64(msg_vec[0]) % 16;
 
-        msg!("msg_type: => {} {}", msg_type, vec_to_u64(msg_vec[0]));
-        if msg_type == 1u64{ // B->A
-            let authority = remaining_accounts[0].to_account_info();
-            let stake_acc = remaining_accounts[1].to_account_info();
-            let token_acc = remaining_accounts[2].to_account_info();
+        let authority = remaining_accounts[0].to_account_info();
+        let stake_acc = remaining_accounts[1].to_account_info();
+        let token_acc = remaining_accounts[2].to_account_info();
 
-            // ---------------------Transfer the source token from the staking account----------------------------------
-            let cpi_accounts = Transfer {
-                from: stake_acc,
-                to: token_acc,
-                authority: authority
-            };
+        // ---------------------Transfer the source token from the staking account----------------------------------
+        let cpi_accounts = Transfer {
+            from: stake_acc,
+            to: token_acc,
+            authority: authority
+        };
 
-            let cpi_context = CpiContext::new(remaining_accounts[3].to_account_info(), cpi_accounts);
-            token::transfer(cpi_context.with_signer(signer_seeds), trade_match.source_sell_amount)?;
-        } 
-        else { // B->A->B
-            let receiver = msg_vec[1];
-            let mut remaining_accounts = ctx.remaining_accounts.to_vec();
-            // let tristero_oapp = accounts[0].clone();
-            let endpoint_program_id = remaining_accounts[7].clone();
-            let signer1 = remaining_accounts[11].clone();
-            let send_library_program_id = remaining_accounts[14].clone();
-            let price_fee_program_id = remaining_accounts[17].clone();
-            let price_fee_program_pda = remaining_accounts[18].clone();
-            remaining_accounts.insert(0, endpoint_program_id);
-            // remaining_accounts.insert(1, tristero_oapp);
+        let cpi_context = CpiContext::new(remaining_accounts[3].to_account_info(), cpi_accounts);
+        token::transfer(cpi_context.with_signer(signer_seeds), trade_match.source_sell_amount)?;
 
-            remaining_accounts.insert(2, send_library_program_id);
-            remaining_accounts.insert(13, signer1);
-            remaining_accounts.extend_from_slice(&[
-                price_fee_program_id,
-                price_fee_program_pda
-            ]);
-
-            // --------------------------Send message through Oapp-----------------------------
-            let cpi_ctx = Send::construct_context(remaining_accounts[9].key(), remaining_accounts.as_ref()).unwrap();
-
-            let receive_options= [0, 3, 1, 0, 17, 1, 0,
-                    0, 0, 0, 0,  0, 0,   0,
-                    0, 0, 0, 0,  0, 7, 161,
-                    32]; // For lzReceiveOption
-            let sol_eid: u32 = 40168u32; // testnet(mainnet: 30168)
-
-            //message: to send to arbitrum
-            let mut message_to_send = Vec::<u8>::new();
-
-            //payload
-            for _ in 0..32 { // sender
-                message_to_send.push(0u8);
-            }
-            for _ in 0..28 {
-                message_to_send.push(0u8);
-            }
-            sol_eid.to_be_bytes().map(|value: u8| message_to_send.push(value)); // srcLzc
-            
-            for _ in 0..12 {
-                message_to_send.push(0u8);
-            }
-            trade_match.dest_token_mint.map(|value| message_to_send.push(value)); // erc20Token
-            trade_match.source_token_mint.to_bytes().map(|value| message_to_send.push(value)); // splToken
-            
-            for _ in 0..24 {
-                message_to_send.push(0u8);
-            }
-            trade_match.dst_index.to_be_bytes().map(|value| message_to_send.push(value)); // srcIndex(arb index)
-
-            for _ in 0..24 {
-                message_to_send.push(0u8);
-            }
-            trade_match.src_index.to_be_bytes().map(|value| message_to_send.push(value)); // dstIndex(sol index)
-
-            // let receiver = [0u8; 32];
-            receiver.map(|value| message_to_send.push(value)); // taker
-            for _ in 0..24 {
-                message_to_send.push(0u8); 
-            }
-            trade_match.source_sell_amount.to_be_bytes().map(|value| message_to_send.push(value)); // minAmount
-            for _ in 0..31 {
-                message_to_send.push(0u8);
-            }
-            message_to_send.push(1u8); //status
-
-            for _ in 0..31 {
-                message_to_send.push(0u8);
-            }
-            message_to_send.push(1u8); // _msgType
-
-            let cpi_params = SendParams {
-                dst_eid: trade_match.eid,
-                receiver: receiver,
-                message: message_to_send, 
-                options: receive_options.to_vec(),
-                native_fee: LAMPORTS_PER_SOL * 3,
-                lz_token_fee: 0,
-            };
-            
-            endpoint::cpi::send(cpi_ctx.with_signer(signer_seeds), cpi_params)?;
-        }
-
-        trade_match.status += 1u8;
+        trade_match.status = 2u8;
         Ok(())
     }
 }
--- programs/tristero/src/instructions/oracle_instruction/lz_receive_types.rs
+++ programs/tristero/src/instructions/oracle_instruction/lz_receive_types.rs
@@ -29,23 +29,15 @@ impl LzReceiveTypes<'_> {
     ) -> Result<Vec<LzAccount>> {
         let program_id = ctx.program_id;
 
-        let (sol_treasury, _) = Pubkey::find_program_address(
-            &[b"sol_treasury"],
-            &program_id,
-        );
-
         let (tristero_oapp, _) = Pubkey::find_program_address(
             &[b"TristeroOapp"], 
             &program_id
         );
 
         /* analyze msg from arb, msg consists of trade_match_id, to_token_address
-        0: msgType
-        1: tradeMatchID,
-        2: destAddr
-        3: destMint
-        4: sender_eid,
-        5: sender
+        0: trade_match_id & msgTyp
+        1: destTokenAddr,
+        2: destTokenMint
         */
         let msg_vec:Vec<[u8; 32]> = split_into_chunks(params.message.clone());
         msg!("==> {:#?}", msg_vec.len());
@@ -58,126 +50,24 @@ impl LzReceiveTypes<'_> {
             &[b"trade_match", &trade_match_id.to_be_bytes()],
             &program_id
         );
+        
+        let to_token_addr = Pubkey::new_from_array(msg_vec[1]);
+        let token_mint = Pubkey::new_from_array(msg_vec[2]);
 
-        if msg_type == 1u64 {
-            let to_token_addr = Pubkey::new_from_array(msg_vec[1]);
-            let token_mint = Pubkey::new_from_array(msg_vec[2]);
+        let (staking_account, _) = Pubkey::find_program_address(
+            &[b"staking_account", &token_mint.to_bytes()],
+            &program_id
+        );
 
-            let (staking_account, _) = Pubkey::find_program_address(
-                &[b"staking_account", &token_mint.to_bytes()],
-                &program_id
-            );
-
-            accounts = vec![
-                LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true }, //0
-                LzAccount { pubkey: trade_match, is_signer: false, is_writable: true }, // 1
-                LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true }, //2
-                LzAccount { pubkey: staking_account, is_signer: false, is_writable: true }, // 3
-                LzAccount { pubkey: to_token_addr, is_signer: false, is_writable: true }, // 4
-                LzAccount { pubkey: TOKEN_PROGRAM_ID, is_signer: false, is_writable: false }, // 5
-            ];
-        } else if msg_type == 2u64 {
-            let sender_addr = msg_vec[1];
-
-            // From here, handle remaining accounts
-            let endpoint_program_id = Pubkey::from_str("76y77prsiCMvXMjuoZ5VRrhG5qYBrUMYTE5WgHqgjEn6").unwrap(); //ok 0
-            
-            let send_library_program = Pubkey::from_str("7a4WjyR8VZ7yZz5XJAKm39BUGn5iT9CKcv2pmG9tdXVH").unwrap(); //ok 2
-            let (send_library_config, _) = Pubkey::find_program_address( //ok 3
-                &[b"SendLibraryConfig", tristero_oapp.key().as_ref(), &sender_eid.to_be_bytes()],
-                &endpoint_program_id
-            );
-            let (default_send_library_config, _) = Pubkey::find_program_address( //ok 4
-                &[b"SendLibraryConfig", &sender_eid.to_be_bytes()],
-                &endpoint_program_id
-            );
-            
-            // let (send_library_info, _) = Pubkey::find_program_address( // ok 5
-            //     &[b"MessageLib", ctx.accounts.message_lib.key().as_ref()],
-            //     &endpoint_program_id
-            // );
-            let send_library_info = ctx.accounts.message_lib.key();
-
-            let (endpoint_pda, _) = Pubkey::find_program_address( // ok 6
-                &[b"Endpoint"],
-                &endpoint_program_id
-            );
-            let (nonce_pda, _) = Pubkey::find_program_address( // ok 7
-                &[b"Nonce", tristero_oapp.key().as_ref(), &sender_eid.to_be_bytes(), sender_addr.as_ref()],
-                &endpoint_program_id
-            );
-            let (event_authority, _) = Pubkey::find_program_address( // ok 8
-                &[b"__event_authority"],
-                &endpoint_program_id
-            );
-            let (uln_program_pda, _) = Pubkey::find_program_address( // ok 10
-                &[b"MessageLib"], 
-                &send_library_program
-            );
-            let (send_config, _) = Pubkey::find_program_address( // ok 11
-                &[b"SendConfig", &sender_eid.to_be_bytes(), tristero_oapp.key().as_ref()], 
-                &send_library_program
-            );
-            let (default_send_config, _) = Pubkey::find_program_address( // ok 12
-                &[b"SendConfig", &sender_eid.to_be_bytes()], 
-                &send_library_program
-            );
-            let system_program_id = SYSTEM_ID; // ok 15
-            let (uln_authority, _) = Pubkey::find_program_address( // ok 16
-                &[b"__event_authority"],
-                &send_library_program
-            );
-            // let send_library_program ok 17
-            let executor_program_id = Pubkey::from_str("6doghB248px58JSSwG4qejQ46kFMW4AMj7vzJnWZHNZn").unwrap(); // ok 18
-            let (executor_pda_deriver, _) = Pubkey::find_program_address( // ok 19
-                &[b"ExecutorConfig"],
-                &executor_program_id
-            );
-            let price_fee_program_id = Pubkey::from_str("8ahPGPjEbpgGaZx2NV1iG5Shj7TDwvsjkEDcGWjt94TP").unwrap(); // ok 20
-            let (price_fee_program_pda, _) = Pubkey::find_program_address( // ok 21
-                &[b"PriceFeed"],
-                &price_fee_program_id
-            );
-            let dvn_program_id = Pubkey::from_str("HtEYV4xB4wvsj5fgTkcfuChYpvGYzgzwvNhgDZQNh7wW").unwrap(); // ok 22
-            let (dvn_derive_config, _) = Pubkey::find_program_address( // ok 23
-                &[b"DvnConfig"],
-                &dvn_program_id
-            );
-            //context accounts
-            accounts.extend_from_slice(&[
-                LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true },
-                LzAccount { pubkey: trade_match, is_signer: false, is_writable: true },
-            ]);
-            //remaining accounts
-            accounts.extend_from_slice(&[
-                // LzAccount { pubkey: endpoint_program_id, is_signer: false, is_writable: true },
-                LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true },
-                // LzAccount { pubkey: send_library_program, is_signer: false, is_writable: true },
-                LzAccount { pubkey: send_library_config, is_signer: false, is_writable: true },
-                LzAccount { pubkey: default_send_library_config, is_signer: false, is_writable: true },
-                LzAccount { pubkey: send_library_info, is_signer: false, is_writable: true },
-                LzAccount { pubkey: endpoint_pda, is_signer: false, is_writable: true },
-                LzAccount { pubkey: nonce_pda, is_signer: false, is_writable: true },
-                LzAccount { pubkey: event_authority, is_signer: false, is_writable: true },
-                LzAccount { pubkey: endpoint_program_id, is_signer: false, is_writable: true }, 
-                LzAccount { pubkey: uln_program_pda, is_signer: false, is_writable: true },
-                LzAccount { pubkey: send_config, is_signer: false, is_writable: true },
-                LzAccount { pubkey: default_send_config, is_signer: false, is_writable: true },
-                // LzAccount { pubkey: signer1, is_signer: false, is_writable: true },
-                LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true },
-                LzAccount { pubkey: system_program_id, is_signer: false, is_writable: true },
-                LzAccount { pubkey: uln_authority, is_signer: false, is_writable: true },
-                LzAccount { pubkey: send_library_program, is_signer: false, is_writable: true },
-                LzAccount { pubkey: executor_program_id, is_signer: false, is_writable: true },
-                LzAccount { pubkey: executor_pda_deriver, is_signer: false, is_writable: true },
-                LzAccount { pubkey: price_fee_program_id, is_signer: false, is_writable: true },
-                LzAccount { pubkey: price_fee_program_pda, is_signer: false, is_writable: true },
-                LzAccount { pubkey: dvn_program_id, is_signer: false, is_writable: true },
-                LzAccount { pubkey: dvn_derive_config, is_signer: false, is_writable: true },
-                // LzAccount { pubkey: price_fee_program_id, is_signer: false, is_writable: true },
-                // LzAccount { pubkey: price_fee_program_pda, is_signer: false, is_writable: true },
-            ]);  
-        }
+        accounts = vec![
+            LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true }, //0
+            LzAccount { pubkey: trade_match, is_signer: false, is_writable: true }, // 1
+            LzAccount { pubkey: tristero_oapp, is_signer: false, is_writable: true }, //2
+            LzAccount { pubkey: staking_account, is_signer: false, is_writable: true }, // 3
+            LzAccount { pubkey: to_token_addr, is_signer: false, is_writable: true }, // 4
+            LzAccount { pubkey: TOKEN_PROGRAM_ID, is_signer: false, is_writable: false }, // 5
+        ];
+        
         Ok(accounts)
     }
 }
