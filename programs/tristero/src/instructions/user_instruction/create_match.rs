1062326c7cb044ba1d83ed24db876799cfbd296b
RustChainBuilder <freshtopromise@gmail.com>
2024-08-27 09:23:31 -0500

    update: create_match instruction

--- programs/tristero/src/instructions/user_instruction/create_match.rs
+++ programs/tristero/src/instructions/user_instruction/create_match.rs
@@ -69,6 +69,7 @@ pub fn create_match(ctx: Context<CreateMatch>, params: &CreateMatchParams) -> Re
     trade_match.status = 0u8;
     
     admin_panel.match_count += 1;
+    order.settled += params.src_quantity;
 
     Ok(())
 }
