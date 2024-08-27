f7566f13264b524eacdfde6593af44b32eb44e06
RustChainBuilder <freshtopromise@gmail.com>
2024-08-27 12:45:45 -0500

    update: finish_challenge instruction

--- programs/tristero/src/instructions/user_instruction/finish_challenge.rs
+++ programs/tristero/src/instructions/user_instruction/finish_challenge.rs
@@ -39,37 +39,23 @@ pub struct FinishChallenge<'info> {
         constraint = arb_user_token_account.key() == trade_match.arb_user_token_account @ CustomError::InvalidTokenOwner
     )]
     pub arb_user_token_account: Box<Account<'info, TokenAccount>>,
-
-    #[account(
-        mut,
-        seeds = [b"staking_account", arb_user_token_account.mint.as_ref()],
-        bump,
-    )]
-    pub staking_account: Box<Account<'info, TokenAccount>>,
-
-    pub system_program: Program<'info, System>,
-
-    /// CHECK: This is not dangerous because we don't read or write from this account
-    #[account(constraint = token_program.key() == TOKEN_PROGRAM_ID @ CustomError::InvalidTokenStandard)]
-    pub token_program: AccountInfo<'info>,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct FinishChallengeParams {
     pub trade_match_id: u64,
-    pub tristero_oapp_bump: u8, 
-    pub source_token_address_in_arbitrum_chain:[u8; 20],
     pub receiver:[u8; 32]
 }
 
 pub fn finish_challenge(ctx: Context<FinishChallenge>, params: &FinishChallengeParams) -> Result<()>  {
 
     let trade_match = ctx.accounts.trade_match.as_mut();
+    let receipt = &ctx.remaining_accounts[0];
 
     // --------------------------Send message through Oapp-----------------------------
     let cpi_ctx = Send::construct_context(ctx.remaining_accounts[9].key(), ctx.remaining_accounts).unwrap();
 
-    let signer_seeds: &[&[&[u8]]] = &[&[b"TristeroOapp", &[params.tristero_oapp_bump]]];
+    let signer_seeds: &[&[&[u8]]] = &[&[b"TristeroOapp", &[ctx.bumps.oapp]]];
 
     let receive_options= [0, 3, 1, 0, 17, 1,  0,
             0, 0, 0, 0,  0, 0,   0,
@@ -78,51 +64,31 @@ pub fn finish_challenge(ctx: Context<FinishChallenge>, params: &FinishChallengeP
 
     let sol_eid: u32 = 40168u32; // testnet(if mainnet => 30168)
 
-    //message: to send to arbitrum
+    //message: to send to arbitrum(trade_match_id, msg_type)
     let mut message_to_send = Vec::<u8>::new();
     
-    // payload
-    for _ in 0..32 { // sender
-        message_to_send.push(0u8);
+    let receipt_state = Receipt::try_from_slice(&receipt.data.borrow());
+    let mut msg_type = 2u8;
+    match receipt_state {
+        Ok(receipt) => {
+            if !receipt.is_valuable {
+                msg_type = 3u8;
+            }
+        },
+        Err(_) => {
+            msg_type = 3u8;
+        }
     }
+
     for _ in 0..28 {
         message_to_send.push(0u8);
     }
-    sol_eid.to_be_bytes().map(|value: u8| message_to_send.push(value)); //srcLzc
-    
-    for _ in 0..12 {
-        message_to_send.push(0u8);
-    }
-    trade_match.dest_token_mint.map(|value| message_to_send.push(value)); // erc20Token
-
-    trade_match.source_token_mint.to_bytes().map(|value| message_to_send.push(value)); // splToken
-    for _ in 0..24 {
-        message_to_send.push(0u8);
-    }
-    trade_match.dst_index.to_be_bytes().map(|value| message_to_send.push(value)); // srcIndex(arb index)
-
-    for _ in 0..24 {
-        message_to_send.push(0u8);
-    }
-    trade_match.trade_match_id.to_be_bytes().map(|value| message_to_send.push(value)); // dstIndex(sol index)
-
-    for _ in 0..12 {
-        message_to_send.push(0u8);
-    }
-    params.source_token_address_in_arbitrum_chain.map(|value| message_to_send.push(value)); // taker
-    for _ in 0..24 {
-        message_to_send.push(0u8); 
-    }
-    trade_match.source_sell_amount.to_be_bytes().map(|value| message_to_send.push(value)); // minAmount
-    for _ in 0..31 {
-        message_to_send.push(0u8);
-    }
-    message_to_send.push(1u8); //status
+    params.trade_match_id.to_be_bytes().map(|value| message_to_send.push(value));
 
     for _ in 0..31 {
         message_to_send.push(0u8);
     }
-    message_to_send.push(2u8); // _msgType
+    message_to_send.push(msg_type); // _msgType
 
     let cpi_params = SendParams {
         dst_eid: trade_match.eid,
@@ -134,17 +100,5 @@ pub fn finish_challenge(ctx: Context<FinishChallenge>, params: &FinishChallengeP
     };
     
     endpoint::cpi::send(cpi_ctx.with_signer(signer_seeds), cpi_params)?;
-
-    // ---------------------Transfer the source token from the staking account----------------------------------
-    let cpi_accounts = Transfer {
-        from: ctx.accounts.staking_account.to_account_info(),
-        to: ctx.accounts.arb_user_token_account.to_account_info(),
-        authority: ctx.accounts.oapp.to_account_info()
-    };
-
-    let cpi_context = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
-    token::transfer(cpi_context.with_signer(signer_seeds), trade_match.source_sell_amount)?;
-
-    trade_match.status = 2u8;
     Ok(())
 }
